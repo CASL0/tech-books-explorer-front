b624938186dc103f06a2fc3aa3ff10a6505b748f
CASL0 <gin.0046.822@gmail.com>
2024-01-28 04:35:17 +0900

    feat: 本の概要を記載するカード

---
+++ src/components/BookCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card :style="style">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ publishedAt }}</v-card-text>
+    <v-card-text>{{ publisher }}</v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  /**
+   * 本のタイトル
+   */
+  title: string;
+  /**
+   * 出版日
+   */
+  publishedAt: string;
+  /**
+   * 出版社
+   */
+  publisher: string;
+  /**
+   * カード背景色
+   */
+  backgroundColor?: string;
+  /**
+   * カード文字色
+   */
+  fontColor?: string;
+}>();
+
+const style = computed(() => ({
+  backgroundColor: props.backgroundColor,
+  color: props.fontColor,
+}));
+</script>
