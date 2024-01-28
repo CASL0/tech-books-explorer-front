f265ebd5f204d75f74b2883f4714313969815f71
CASL0 <gin.0046.822@gmail.com>
2024-01-29 01:49:06 +0900

    feat: 書籍情報取得用のストア

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,64 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import { Book } from "@/models/Book";
+import Papa from "papaparse";
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
+export const useAppStore = defineStore("app", {
+  state: (): { books: Book[] } => ({
+    books: new Array<Book>(),
   }),
-})
+  getters: {
+    /**
+     * オライリー本を取得します
+     * @param state
+     * @returns
+     */
+    oreillyBooks(state: { books: Book[] }): Book[] {
+      return state.books.filter((e) => e.publisher === "オライリー・ジャパン");
+    },
+    /**
+     * 翔泳社の本を取得します
+     * @param state
+     * @returns
+     */
+    shoeishaBooks(state: { books: Book[] }): Book[] {
+      return state.books.filter((e) => e.publisher === "翔泳社");
+    },
+    /**
+     * 技術評論社の本を取得します
+     * @param state
+     * @returns
+     */
+    gihyoBooks(state: { books: Book[] }): Book[] {
+      return state.books.filter((e) => e.publisher === "技術評論社");
+    },
+  },
+  actions: {
+    async fetch(): Promise<void> {
+      Papa.parse("./tech-books.csv", {
+        header: true,
+        delimiter: ",",
+        download: true,
+        step: (row: {
+          data: {
+            isbn: string;
+            price: string;
+            published_at: string;
+            publisher: string;
+            title: string;
+            url: string;
+          };
+        }) => {
+          this.books.push({
+            title: row.data.title,
+            price: row.data.price,
+            isbn: row.data.isbn,
+            publisher: row.data.publisher,
+            publishedAt: row.data.published_at,
+            url: row.data.url,
+          });
+        },
+      });
+    },
+  },
+});
