ea182a4b90725d439cd7762799b52141a70dab00
CASL0 <gin.0046.822@gmail.com>
2024-01-29 01:46:18 +0900

    chore: vue-router/autoとgenerated-layoutsの依存解決

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
+/// <reference types="unplugin-vue-router/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
