5a6ea7307756fbbc206954fdd34bd86943d6532c
CASL0 <gin.0046.822@gmail.com>
2024-01-28 14:27:28 +0900

    feat: 本のモデル

---
+++ src/models/Book.ts
@@ -0,0 +1,21 @@
+/**
+ * 本のモデル
+ */
+export interface Book {
+  /**
+   * 本のタイトル
+   */
+  title: string;
+  /**
+   * 出版日
+   */
+  publishedAt: string;
+  /**
+   * 出版社
+   */
+  publisher: string;
+  /**
+   * isbn
+   */
+  isbn: string;
+}
