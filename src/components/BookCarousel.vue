cd0b0b70b565f454ba7f514b3d8032d2bdfca00c
CASL0 <gin.0046.822@gmail.com>
2024-01-29 02:46:44 +0900

    feat: ホーム画面で各出版社の最近の出版本のカールセルを表示

--- src/components/BookCarousel.vue
+++ src/components/BookCarousel.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-row class="label"> {{ label }} </v-row>
-  <Swiper
-    class="swiper swiper-container"
-    :slidesPerView="3"
-    :spaceBetween="16"
-    :pagination="{
-      clickable: true,
-    }"
-    :modules="modules"
-  >
-    <SwiperSlide v-for="book in books" :key="book.isbn" class="slide">
-      <BookCard
-        :title="book.title"
-        :publishedAt="book.publishedAt"
-        :publisher="book.publisher"
-      />
-    </SwiperSlide>
-  </Swiper>
+  <div>
+    <v-row class="label"> {{ label }} </v-row>
+    <Swiper
+      class="swiper swiper-container"
+      :slidesPerView="3"
+      :spaceBetween="16"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+    >
+      <SwiperSlide v-for="book in books" :key="book.isbn" class="slide">
+        <BookCard
+          :title="book.title"
+          :publishedAt="book.publishedAt"
+          :publisher="book.publisher"
+        />
+      </SwiperSlide>
+    </Swiper>
+  </div>
 </template>
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,57 @@
 <template>
-  <HelloWorld />
+  <BookCarousel
+    :books="oreillyBooks"
+    :label="$t('bookCarousel.oreilly')"
+    class="carousel"
+  />
+  <BookCarousel
+    :books="gihyoBooks"
+    :label="$t('bookCarousel.gihyo')"
+    class="carousel"
+  />
+  <BookCarousel
+    :books="shoeishaBooks"
+    :label="$t('bookCarousel.shoeisha')"
+    class="carousel"
+  />
 </template>
 
 <script lang="ts" setup>
-  //
+import BookCarousel from "@/components/BookCarousel.vue";
+import { computed, onMounted } from "vue";
+import { useAppStore } from "@/store/app";
+import dayjs from "dayjs";
+import { Book } from "@/models/Book";
+
+const appStore = useAppStore();
+
+onMounted(async () => {
+  await appStore.fetch();
+});
+
+const oreillyBooks = computed<Book[]>(() => {
+  const threeMonthsAgo = dayjs().subtract(3, "month");
+  return appStore.oreillyBooks
+    .filter((e: Book) => e.publishedAt >= threeMonthsAgo.format("YYYY-MM-DD"))
+    .slice(0, 10);
+});
+
+const gihyoBooks = computed<Book[]>(() => {
+  const threeMonthsAgo = dayjs().subtract(3, "month");
+  return appStore.gihyoBooks
+    .filter((e: Book) => e.publishedAt >= threeMonthsAgo.format("YYYY-MM-DD"))
+    .slice(0, 10);
+});
+
+const shoeishaBooks = computed<Book[]>(() => {
+  const threeMonthsAgo = dayjs().subtract(3, "month");
+  return appStore.shoeishaBooks
+    .filter((e: Book) => e.publishedAt >= threeMonthsAgo.format("YYYY-MM-DD"))
+    .slice(0, 10);
+});
 </script>
+<style scoped>
+.carousel {
+  margin: 0 16px 32px 16px;
+}
+</style>
