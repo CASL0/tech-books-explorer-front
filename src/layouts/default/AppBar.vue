2d3b541f22ef8424edfcefc7c41ea449e77b6dbb
CASL0 <gin.0046.822@gmail.com>
2024-01-30 12:33:12 +0000

    fix: AppBarにキーワード検索用のinputを追加

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,64 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
+    <div class="title">
       <v-icon icon="mdi-book-search" />
 
       {{ $t("appName") }}
-    </v-app-bar-title>
+    </div>
+    <v-spacer />
+    <form class="search-form" @submit.prevent="onSubmit(keyword)">
+      <input
+        type="text"
+        v-model="keyword"
+        class="text-field"
+        placeholder="キーワードで検索"
+      />
+    </form>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-//
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const keyword = ref("");
+const onSubmit = (keyword: string) => {
+  router.push({
+    path: "/search",
+    query: {
+      keyword,
+    },
+  });
+};
 </script>
+<style scoped>
+.title {
+  margin-left: 16px;
+  font-size: 20px;
+}
+
+.search-form {
+  margin-right: 16px;
+
+  .text-field {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 8px 12px;
+    font-size: 16px;
+    background-color: #fff;
+    color: #333;
+    outline: none;
+    transition: border-color 0.3s;
+    width: 250px;
+  }
+
+  .text-field:focus {
+    border-color: #007bff;
+  }
+
+  .text-field::placeholder {
+    color: #aaa;
+  }
+}
+</style>
